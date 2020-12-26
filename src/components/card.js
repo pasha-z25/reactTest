bcb08001f3f9ea613ec104925bf829909fc3ff55
Pasha <pashazubak@gmail.com>
2020-12-26 14:55:10 +0200

    add click action

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,29 @@
 import React from "react";
+import store from '../store';
 import { observer } from "mobx-react";
 
 class Card extends React.Component {
 
     render(props) {
         let oneCard = this.props.oneCard
+        let { cards, setModal, addCardToModal } = store;
+
+        function getCurrentCard() {
+            let current = cards.filter( item => item.name === oneCard.name);
+            return current[0];
+        }
+
+        function buyThisCard () {
+            addCardToModal(getCurrentCard());
+            setModal(true);
+        }
 
         return (
-            <div className={"card"}>
+            <div className="card transition" onClick={ buyThisCard }>
                 <p>{ oneCard.category }</p>
                 <p>{ oneCard.name }</p>
-                <p>{ oneCard.price }</p>
+                <p><sup>$</sup>{ oneCard.price }</p>
+                <button className="btn uppercase">Buy</button>
             </div>
         );
     }
