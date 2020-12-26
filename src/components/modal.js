be04118cc914cdae29f110d72065dc105d7f7ace
Pasha <pashazubak@gmail.com>
2020-12-26 14:54:38 +0200

    fixed modal view and actions

--- src/components/modal.js
+++ src/components/modal.js
@@ -6,24 +6,34 @@ class Modal extends React.Component {
 
     render(props) {
 
-        let { modalState, setModal } = store;
+        let { modalState, setModal, modalCard, clearModalCard } = store;
 
         function hideModal() {
             setModal(false);
+            clearModalCard();
         }
 
         return (
             <div className={`modal-wrap ${ modalState ? 'active' : ''}`}>
+
                 <form className="modal-body" onSubmit={ event => { event.preventDefault(); console.log('Submit') }}>
-                    <p>Title</p>
-                    <p>
-                        <label>
-                            <input type="text"/>
-                        </label>
-                    </p>
-                    <p>
-                        <button onClick={ hideModal }>Hide Modal</button>
-                    </p>
+                    <span className="close transition" onClick={ hideModal }>&times;</span>
+                    { modalCard.map( (item, index) => {
+                        return (
+                            <div className="oneItem text-center" key={ index }>
+                                <p className="category">{ item.category }</p>
+                                <p className="name">{ item.name }</p>
+                                <p className="price"><sup>$</sup>{ item.price }</p>
+                            </div>
+                        )
+                    })}
+                    <label>
+                        <input type="text"/>
+                    </label>
+                    <label>
+                        <input type="tel"/>
+                    </label>
+                    <button className="btn btn-default submitBtn uppercase" type="submit">Order</button>
                 </form>
 
             </div>
