de85b9792ab4d9ced71af510c4b05ce0c5866de2
Pasha <pashazubak@gmail.com>
2020-12-27 13:38:58 +0200

    add validation functions

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,9 +8,31 @@ class Modal extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            names: "chmj",
             name: "",
-            number: ""
+            number: "",
+            nameErrors: [],
+            numberErrors: []
+        }
+    }
+
+    validateName = () => {
+        if (this.state.name.trim() === '') {
+            this.state.nameErrors.push('This field in required')
+        }
+        if ( !(/^[a-zA-Zа-яА-Я]*$/.test(this.state.name)) && this.state.name.length ) {
+            this.state.nameErrors.push('Only letters allowed')
+        }
+    }
+
+    validateNumber = () => {
+        if (this.state.number.trim() === '') {
+            this.state.numberErrors.push('This field in required')
+        }
+        if ( !(/^[0-9]*$/.test(this.state.number)) && this.state.number.length ) {
+            this.state.numberErrors.push('Only numbers allowed')
+        }
+        if (this.state.number.length !== 12) {
+            this.state.numberErrors.push('Should contain 12 characters')
         }
     }
 
@@ -18,6 +40,12 @@ class Modal extends React.Component {
         const name = e.target.name;
         const value = e.target.value;
         this.setState({[name]: value});
+        if (name === 'name') {
+            this.setState({ nameErrors: []})
+        }
+        if (name === 'number') {
+            this.setState({ numberErrors: []})
+        }
     }
 
     validateForm = () => {
@@ -39,7 +67,6 @@ class Modal extends React.Component {
     }
 
     render() {
-
         let { modalState,  modalCard } = store;
 
         return (
@@ -48,18 +75,34 @@ class Modal extends React.Component {
                     <span className="close transition" onClick={ this.hideModal }>&times;</span>
                     { modalCard.map( (item, index) => {
                         return (
-                            <div className="oneItem text-center" key={ index }>
-                                <p className="category">{ item.category }</p>
-                                <p className="name">{ item.name }</p>
+                            <div className="oneItem modal-item text-center" key={ index }>
+                                <p className="category uppercase">{ item.category }</p>
+                                <p className="name capitalize">{ item.name }</p>
                                 <p className="price"><sup>$</sup>{ item.price }</p>
                             </div>
                         )
                     })}
 
-                    <FormGroup type={"text"} name={"name"} value={this.state.name} onChange={ this.onInputChange } />
-                    <FormGroup type={"tel"} name={"number"} value={this.state.number} onChange={ this.onInputChange } />
-                    <span>{`Name: ${this.state.name}, number: ${this.state.number}, ${this.state.names}`}</span>
-                    <button className="btn btn-default submitBtn uppercase" type="submit">Order</button>
+                    <FormGroup type={"text"} name={"name"} placeholder={"Name"}
+                               value={this.state.name}
+                               onError={this.state.nameErrors}
+                               onChange={ this.onInputChange }
+                               onBlur={ this.validateName }
+                    />
+
+                    <FormGroup type={"tel"} name={"number"} placeholder={"Number"}
+                               value={this.state.number}
+                               onError={this.state.numberErrors}
+                               onChange={ this.onInputChange }
+                               onBlur={ this.validateNumber }
+                    />
+
+                    <span className="w-100">{` ${this.state.name} and ${this.state.number} `}</span>
+
+                    <div className="text-center">
+                        <button className="btn btn-default w-100 submitBtn uppercase" type="submit">Order</button>
+                    </div>
+
                 </form>
             </div>
         );
